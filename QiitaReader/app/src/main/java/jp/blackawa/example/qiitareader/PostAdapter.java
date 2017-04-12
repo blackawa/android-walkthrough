faab27a7425d5f1b987857967fd6ec617680bc1b
blackawa <3104kurokawa@gmail.com>
2017-04-12 16:59:33 +0900

    DataBindingでView表示

--- QiitaReader/app/src/main/java/jp/blackawa/example/qiitareader/PostAdapter.java
+++ QiitaReader/app/src/main/java/jp/blackawa/example/qiitareader/PostAdapter.java
@@ -1,15 +1,16 @@
 package jp.blackawa.example.qiitareader;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import java.util.List;
 
 import jp.blackawa.example.qiitareader.data.Post;
+import jp.blackawa.example.qiitareader.databinding.PostListItemBinding;
 
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.VH> {
 
@@ -38,29 +39,24 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.VH> {
     @Override
     public void onBindViewHolder(VH holder, int position) {
         Post post = posts.get(position);
-        holder.bind(post.getTitle(), post.getUsername(), post.getCreatedAt());
+        holder.getBinding().setPost(post);
     }
 
     @Override
     public int getItemCount() {
-        return posts == null? 0 : posts.size();
+        return posts == null ? 0 : posts.size();
     }
 
     class VH extends RecyclerView.ViewHolder {
-        private TextView title;
-        private TextView username;
-        private TextView postDate;
-        VH(View itemView) {
-            super(itemView);
-            title = (TextView) itemView.findViewById(R.id.post_title);
-            username = (TextView) itemView.findViewById(R.id.username);
-            postDate = (TextView) itemView.findViewById(R.id.post_date);
+        private PostListItemBinding mBinding;
+
+        VH(View v) {
+            super(v);
+            mBinding = DataBindingUtil.bind(v);
         }
 
-        void bind(String title, String username, String postDate) {
-            this.title.setText(title);
-            this.username.setText(username);
-            this.postDate.setText(postDate);
+        PostListItemBinding getBinding() {
+            return mBinding;
         }
     }
 }
