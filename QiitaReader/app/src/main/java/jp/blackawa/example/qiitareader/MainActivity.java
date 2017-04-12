dd0bdbaf0b1a137fc976309afad1db87197ad77a
blackawa <3104kurokawa@gmail.com>
2017-04-12 15:22:41 +0900

    RxAndroidに置換

--- QiitaReader/app/src/main/java/jp/blackawa/example/qiitareader/MainActivity.java
+++ QiitaReader/app/src/main/java/jp/blackawa/example/qiitareader/MainActivity.java
@@ -1,22 +1,22 @@
 package jp.blackawa.example.qiitareader;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import jp.blackawa.example.qiitareader.data.Post;
 import jp.blackawa.example.qiitareader.entity.PostEntity;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,26 +46,23 @@ public class MainActivity extends AppCompatActivity {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://qiita.com/api/v2/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
 
         QiitaService service = retrofit.create(QiitaService.class);
-        Call<List<PostEntity>> call = service.listItems(1, 20);
+        Observable<List<PostEntity>> observable = service.listItems(1, 20);
 
-        call.enqueue(new Callback<List<PostEntity>>() {
-            @Override
-            public void onResponse(Call<List<PostEntity>> call, Response<List<PostEntity>> response) {
-                Log.d(this.getClass().getSimpleName(), "レスポンスを受け取りました. " + response.body().toString());
-                List<Post> posts = new ArrayList<>();
-                for (PostEntity post : response.body()) {
-                    posts.add(new Post(post.getTitle(), post.getUser().getName(), post.getCreatedAt()));
-                }
-                updateRecyclerView(posts);
-            }
-
-            @Override
-            public void onFailure(Call<List<PostEntity>> call, Throwable t) {
-                Log.e(this.getClass().getSimpleName(), "HTTPリエクストでエラーが発生しました. " + t.getClass().getSimpleName() + ": " + t.getMessage());
-            }
-        });
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<List<PostEntity>>() {
+                    @Override
+                    public void call(List<PostEntity> postEntities) {
+                        List<Post> posts = new ArrayList<>();
+                        for (PostEntity post : postEntities) {
+                            posts.add(new Post(post.getTitle(), post.getUser().getName(), post.getCreatedAt()));
+                        }
+                        updateRecyclerView(posts);
+                    }
+                });
     }
 }
--- QiitaReader/app/src/main/java/jp/blackawa/example/qiitareader/QiitaService.java
+++ QiitaReader/app/src/main/java/jp/blackawa/example/qiitareader/QiitaService.java
@@ -3,13 +3,13 @@ package jp.blackawa.example.qiitareader;
 import java.util.List;
 
 import jp.blackawa.example.qiitareader.entity.PostEntity;
-import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import rx.Observable;
 
 public interface QiitaService {
     @GET("items")
-    Call<List<PostEntity>> listItems(
+    Observable<List<PostEntity>> listItems(
             @Query("page") int page,
             @Query("per_page") int perPage
     );
