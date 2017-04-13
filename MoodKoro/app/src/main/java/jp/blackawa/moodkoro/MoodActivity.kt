9a100e3d4fcb5c9f1e65d968961a16c3860df99d
blackawa <3104kurokawa@gmail.com>
2017-04-13 15:09:07 +0900

    ButtonのonClickListener追加

--- MoodKoro/app/src/main/java/jp/blackawa/moodkoro/MoodActivity.kt
+++ MoodKoro/app/src/main/java/jp/blackawa/moodkoro/MoodActivity.kt
@@ -1,15 +1,28 @@
 package jp.blackawa.moodkoro
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import jp.blackawa.moodkoro.databinding.ActivityMoodBinding
 
 class MoodActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMoodBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mood)
+        binding = DataBindingUtil.setContentView<ActivityMoodBinding>(this, R.layout.activity_mood)
+
+        binding.buttonSaveMood.setOnClickListener {
+            val solution: String = binding.editTextSolution.text.toString()
+            Log.d(this.javaClass.simpleName, "save button clicked.")
+            Toast.makeText(this, "EditText is \"${solution}\"", Toast.LENGTH_LONG).show()
+            finish()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
