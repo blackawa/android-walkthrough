0d7b53cb449bbf6dc8cbac8823a9158026f746b3
blackawa <3104kurokawa@gmail.com>
2017-04-13 10:25:40 +0900

    DataBinding化

--- MoodKoro/app/src/main/java/jp/blackawa/moodkoro/HistoryActivity.java
+++ MoodKoro/app/src/main/java/jp/blackawa/moodkoro/HistoryActivity.java
@@ -1,7 +1,10 @@
 package jp.blackawa.moodkoro;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import jp.blackawa.moodkoro.databinding.ActivityHistoryBinding;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        ActivityHistoryBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_history);
+        binding.setGreet("Hello, world!");
     }
 }
