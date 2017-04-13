9c9a242f505a512d6c78dc1a5406f11b0106ef2f
blackawa <3104kurokawa@gmail.com>
2017-04-13 13:37:37 +0900

    add menu

--- MoodKoro/app/src/main/java/jp/blackawa/moodkoro/HistoryActivity.kt
+++ MoodKoro/app/src/main/java/jp/blackawa/moodkoro/HistoryActivity.kt
@@ -4,6 +4,9 @@ import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import jp.blackawa.moodkoro.adapter.HistoryListItemAdapter
 import jp.blackawa.moodkoro.databinding.ActivityHistoryBinding
 import jp.blackawa.moodkoro.service.MoodService
@@ -17,5 +20,20 @@ class HistoryActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView<ActivityHistoryBinding>(this, R.layout.activity_history)
         binding.recyclerMoods.adapter = HistoryListItemAdapter(MoodService.fetchMoods())
         binding.recyclerMoods.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.history, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_item_add -> {
+                Toast.makeText(this, "ADD button clicked", Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
