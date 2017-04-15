3ea5efc5e635a644760fdc7a5281b083ec5572d4
blackawa <3104kurokawa@gmail.com>
2017-04-15 12:10:37 +0900

    ちょっとキレイにした

--- QiitaReader/app/src/main/java/jp/blackawa/example/qiitareader/MainActivity.kt
+++ QiitaReader/app/src/main/java/jp/blackawa/example/qiitareader/MainActivity.kt
@@ -4,40 +4,31 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-
-import java.util.ArrayList
-
 import jp.blackawa.example.qiitareader.data.Post
-import jp.blackawa.example.qiitareader.entity.PostEntity
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import rx.Observable
 import rx.android.schedulers.AndroidSchedulers
-import rx.functions.Action1
 import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
-    private var mPostRecyclerView: RecyclerView? = null
-    private var mAdapter: PostAdapter? = null
+    private val mPostRecyclerView: RecyclerView = findViewById(R.id.rv_posts) as RecyclerView
+    private var mAdapter: PostAdapter = PostAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mPostRecyclerView = findViewById(R.id.rv_posts) as RecyclerView
+        mPostRecyclerView.layoutManager = LinearLayoutManager(this)
 
-        mPostRecyclerView!!.layoutManager = LinearLayoutManager(this)
-
-        mAdapter = PostAdapter()
-        mPostRecyclerView!!.adapter = mAdapter
+        mPostRecyclerView.adapter = mAdapter
 
         fetchQiitaItems()
     }
 
     private fun updateRecyclerView(posts: List<Post>) {
-        mAdapter!!.setPosts(posts)
+        mAdapter.setPosts(posts)
     }
 
     private fun fetchQiitaItems() {
@@ -47,15 +38,14 @@ class MainActivity : AppCompatActivity() {
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build()
 
-        val service = retrofit.create<QiitaService>(QiitaService::class.java!!)
+        val service = retrofit.create<QiitaService>(QiitaService::class.java)
         val observable = service.listItems(1, 20)
 
         observable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe { postEntities ->
-                    val posts = ArrayList<Post>()
-                    for (post in postEntities) {
-                        posts.add(Post(post.title, post.user.name, post.createdAt))
+                    val posts = postEntities.map {
+                        Post(it.title, it.user.name, it.createdAt)
                     }
                     updateRecyclerView(posts)
                 }
