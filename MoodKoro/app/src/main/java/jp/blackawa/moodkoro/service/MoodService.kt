badf5cfa28bca0f9d780e26917a5f3f680b9ee55
blackawa <3104kurokawa@gmail.com>
2017-04-19 23:08:30 +0900

    使ってないクラス

--- MoodKoro/app/src/main/java/jp/blackawa/moodkoro/service/MoodService.kt
+++
@@ -1,8 +0,0 @@
-package jp.blackawa.moodkoro.service
-
-import jp.blackawa.moodkoro.domain.Mood
-import jp.blackawa.moodkoro.repository.MoodRepository
-
-object MoodService {
-    fun fetchMoods(): List<Mood> = MoodRepository.findAll().map(::Mood)
-}
