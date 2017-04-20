418e9f5c93498fa1d2bccf4a2c1da3bc9792201a
blackawa <3104kurokawa@gmail.com>
2017-04-20 13:22:04 +0900

    画面遷移

--- GitHubRepositorySearcher/app/src/main/java/jp/blackawa/example/githubrepositorysearcher/ResultActivity.java
+++ GitHubRepositorySearcher/app/src/main/java/jp/blackawa/example/githubrepositorysearcher/ResultActivity.java
@@ -1,13 +1,34 @@
 package jp.blackawa.example.githubrepositorysearcher;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import jp.blackawa.example.githubrepositorysearcher.databinding.ActivityResultBinding;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private ActivityResultBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_result);
+        String query = getIntent().getExtras().get("query").toString();
+        binding.setViewModel(new ViewModel(query));
+    }
+
+    public class ViewModel {
+        private String query;
+
+        public ViewModel(String query) {
+            this.query = query;
+        }
+
+        public String getQuery() {
+            return query;
+        }
     }
 }
--- GitHubRepositorySearcher/app/src/main/java/jp/blackawa/example/githubrepositorysearcher/SearchActivity.java
+++ GitHubRepositorySearcher/app/src/main/java/jp/blackawa/example/githubrepositorysearcher/SearchActivity.java
@@ -1,13 +1,39 @@
 package jp.blackawa.example.githubrepositorysearcher;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import jp.blackawa.example.githubrepositorysearcher.databinding.ActivitySearchBinding;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private ActivitySearchBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_search);
+
+        binding.setViewModel(new ViewModel(this));
+    }
+
+    public class ViewModel {
+        private Context context;
+
+        public ViewModel(Context context) {
+            this.context = context;
+        }
+
+        public void onClickSearchButton() {
+            String query = binding.editTextQuery.getText().toString();
+            Intent intent = new Intent(context, ResultActivity.class);
+            intent.putExtra("query", query);
+            startActivity(intent);
+        }
     }
 }
